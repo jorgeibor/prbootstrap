7cc0fc9e7dd471230ef9431ae4f8c996c1621550
jorgeibor <jorgehuetelosenlaces@gmail.com>
2016-02-22 23:49:43 +0100

    2/4 Páginas

---
+++ app/styles/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+
+<link rel="stylesheet" href="css/footer.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<footer class="footer">
+    <div class="container">
+		<a class="aFooter" href="index.php">Inicio</a> | 
+		<a class="aFooter" href="info.php">Información</a> | 
+		<a class="aFooter" href="galeria.php">Galeria</a> | 
+		<a class="aFooter" href="contacto.php">Contacto</a> | 
+		<a class="aFooter disabled" href=""> Los Fugaos © 2016 </a>
+    </div>
+</footer>
